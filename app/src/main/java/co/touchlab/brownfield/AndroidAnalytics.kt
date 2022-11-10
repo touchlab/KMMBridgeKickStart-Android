3f0f5db98ef31021aca7eccdeee4c20e52300884
Julia Jakubcova <j.jakubcova@touchlab.co>
2022-11-10 09:46:46 +0100

    Add a comment about analytics usage.

--- app/src/main/java/co/touchlab/brownfield/AndroidAnalytics.kt
+++ app/src/main/java/co/touchlab/brownfield/AndroidAnalytics.kt
@@ -4,6 +4,7 @@ import co.touchlab.brownfieldsdk.Analytics
 
 class AndroidAnalytics : Analytics {
     override fun sendEvent(eventName: String, eventArgs: Map<String, Any>) {
+        // In a real app, you would call to your analytics backend here
         println("$eventName - $eventArgs")
     }
 }
